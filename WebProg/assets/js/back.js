3a6bf13c9dbf3a9b20388aadeea4920dcfb216e5
Trust <103842703+kiddingtrust@users.noreply.github.com>
2022-04-17 14:08:04 +0400

    Add files via upload

---
+++ WebProg/assets/js/back.js
@@ -0,0 +1,18 @@
+function back_to_top(){
+let button = $('.back_to_top');
+
+ $(window).on('scroll', () => {
+	if($(this).scrollTop() >= 50 ){
+		button.fadeIn();
+	} else {
+		button.fadeOut();
+	}
+});
+
+button.on('click',(e) => {
+	e.preventDefault();
+	$('html').animate({ scrollTop: 0}, 1000);
+})
+}
+
+back_to_top();
